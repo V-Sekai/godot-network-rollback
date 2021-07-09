4e42738f33862bdfedf42d93f64cf7a8016b9eac
David Snopek <dsnopek@gmail.com>
2021-07-09 11:31:29 -0500

    Fix up debug and some little things

--- Main.gd
+++ Main.gd
@@ -1,11 +1,19 @@
 extends Node2D
 
 onready var connection_panel = $CanvasLayer/ConnectionPanel
+onready var message_label = $CanvasLayer/MessageLabel
 
 const SERVER_PORT = 9999
 
 func _ready() -> void:
 	get_tree().connect("network_peer_connected", self, "_on_network_peer_connected")
+	SyncManager.connect("sync_error", self, "_on_SyncManager_sync_error")
+	
+	var cmdline_args = OS.get_cmdline_args()
+	if "server" in cmdline_args:
+		_on_ServerButton_pressed()
+	else:
+		_on_ClientButton_pressed()
 
 func _on_ServerButton_pressed() -> void:
 	var peer = NetworkedMultiplayerENet.new()
@@ -29,6 +37,9 @@ func _on_network_peer_connected(peer_id: int):
 	SyncManager.add_peer(peer_id)
 	if get_tree().is_network_server():
 		SyncManager.start()
-	
-	
 
+func _on_SyncManager_sync_error(msg: String) -> void:
+	message_label.text = "SYNC LOST: " + msg
+
+func _on_ResetButton_pressed() -> void:
+	get_tree().reload_current_scene()
