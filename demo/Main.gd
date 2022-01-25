287e08c0392c5fed5949b7885f574484f179559a
David Snopek <dsnopek@gmail.com>
2022-01-25 10:10:30 -0600

    Improvements to the demo project

--- demo/Main.gd
+++ demo/Main.gd
@@ -1,15 +1,17 @@
 extends Node2D
 
 onready var connection_panel = $CanvasLayer/ConnectionPanel
+onready var host_field = $CanvasLayer/ConnectionPanel/GridContainer/HostField
+onready var port_field = $CanvasLayer/ConnectionPanel/GridContainer/PortField
 onready var message_label = $CanvasLayer/MessageLabel
-
-const SERVER_PORT = 9999
+onready var sync_lost_label = $CanvasLayer/SyncLostLabel
 
 func _ready() -> void:
 	get_tree().connect("network_peer_connected", self, "_on_network_peer_connected")
 	get_tree().connect("network_peer_disconnected", self, "_on_network_peer_disconnected")
 	get_tree().connect("server_disconnected", self, "_on_server_disconnected")
 	SyncManager.connect("sync_started", self, "_on_SyncManager_sync_started")
+	SyncManager.connect("sync_stopped", self, "_on_SyncManager_sync_stopped")
 	SyncManager.connect("sync_lost", self, "_on_SyncManager_sync_lost")
 	SyncManager.connect("sync_regained", self, "_on_SyncManager_sync_regained")
 	SyncManager.connect("sync_error", self, "_on_SyncManager_sync_error")
@@ -22,13 +24,13 @@ func _ready() -> void:
 
 func _on_ServerButton_pressed() -> void:
 	var peer = NetworkedMultiplayerENet.new()
-	peer.create_server(SERVER_PORT, 2)
+	peer.create_server(int(port_field.text), 2)
 	get_tree().network_peer = peer
 	connection_panel.visible = false
 
 func _on_ClientButton_pressed() -> void:
 	var peer = NetworkedMultiplayerENet.new()
-	peer.create_client('127.0.0.1', SERVER_PORT)
+	peer.create_client(host_field.text, int(port_field.text))
 	get_tree().network_peer = peer
 	connection_panel.visible = false
 	message_label.text = "Connecting..."
@@ -57,14 +59,19 @@ func _on_server_disconnected() -> void:
 func _on_SyncManager_sync_started() -> void:
 	message_label.text = "Started!"
 
+func _on_SyncManager_sync_stopped() -> void:
+	pass
+
 func _on_SyncManager_sync_lost() -> void:
-	message_label.text = "Re-syncing..."
+	sync_lost_label.visible = true
 
 func _on_SyncManager_sync_regained() -> void:
-	message_label.text = ''
+	sync_lost_label.visible = false
 
 func _on_SyncManager_sync_error(msg: String) -> void:
 	message_label.text = "Fatal sync error: " + msg
+	sync_lost_label.visible = false
+	
 	var peer = get_tree().network_peer
 	if peer:
 		peer.close_connection()
@@ -72,6 +79,7 @@ func _on_SyncManager_sync_error(msg: String) -> void:
 
 func _on_ResetButton_pressed() -> void:
 	SyncManager.stop()
+	SyncManager.clear_peers()
 	var peer = get_tree().network_peer
 	if peer:
 		peer.close_connection()
